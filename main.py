dd76514372de8ad99ae282e76b070502abd75323
Ehsan Ullah <ihsan42180@gmail.com>
2026-01-11 23:29:35 +0500

    Add themes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import gradio as gr
 from app.ui import create_ui
 
 if __name__ == "__main__":
     demo = create_ui()
-    demo.launch(share=True)
+    demo.launch(share=True, theme=gr.themes.Soft())
